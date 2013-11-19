e4ea5f773dab61c3965a21cc3d651bbefceabe1d
khairaniummah <khairaniummah@yahoo.com>
2013-11-19 08:02:13 +0700

    revisi2

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ ini_set('display_errors', true);
 ini_set('auto_detect_line_endings', true);
 require_once('./Converter.php');
 $proc = new XsltProcessor;
-
+echo 'Untuk melihat tabel gunakan perintah dibawah ini<br>';
+echo 'Perintah1: http://localhost/?search=dataxml<br>';
+echo 'Perintah2: http://localhost/?search=datasql<br>';
+echo 'Perintah3: http://localhost/?search=datacsv<br>';
 $t = new Converter;
-if ($_GET["search"] == 'dataxml'){ //perintah: http://localhost/main.php?search=dataxml
+if ($_GET["search"] == 'dataxml'){ //perintah: http://localhost/get.php?search=dataxml
 	$inputFile = 'Menu.xml';
 	$doc = new DOMDocument();
 	$doc->load('Menu.xsl');
@@ -18,7 +21,7 @@ if ($_GET["search"] == 'dataxml'){ //perintah: http://localhost/main.php?search=
 	//$xml = file_get_contents($inputFile);
 
 }
-else if ($_GET["search"] == 'datacsv'){ //perintah: http://localhost/main.php?search=datacsv
+else if ($_GET["search"] == 'datacsv'){ //perintah: http://localhost/get.php?search=datacsv
 	$convert = $t->csvConverter();
 	$inputFile = 'output.xml';
 	//$xml = file_get_contents($inputFile);
@@ -32,7 +35,7 @@ else if ($_GET["search"] == 'datacsv'){ //perintah: http://localhost/main.php?se
 	echo $proc->transformToXML($doc2);
 }
 
-else if ($_GET["search"] == 'datasql'){ //perintah: http://localhost/main.php?search=datasql
+else if ($_GET["search"] == 'datasql'){ //perintah: http://localhost/get.php?search=datasql
 	$convert = $t->sqlConverter();
 	$doc = new DOMDocument();
 	$doc->load('datasql.xsl');
